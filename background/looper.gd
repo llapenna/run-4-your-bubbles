56b90f3e5f6089f2ac91c02a221981ad55da6719
Luciano Lapenna <lucianoglapenna@gmail.com>
2025-01-25 17:43:16 -0300

    use signal argument instead of last children

--- background/looper.gd
+++ background/looper.gd
@@ -9,28 +9,14 @@ var speed = 1
 func remove_sprite(sprite:Sprite2D):
 	sprite.queue_free()
 
-func spawn_next(_sprite: Sprite2D):
-	var last = get_sprites()[-1]
-	
-	if !last:
-		return
-	
+func spawn_next(sprite: Sprite2D):
 	var instance: Sprite2D = backgroundScene.instantiate()
 	$".".add_child(instance)
 	
-	instance.position.x += last.position.x + get_bg_length() / 2
+	instance.position.x += sprite.position.x + get_bg_length() / 2
 	instance.texture = texture
 	instance.connect("on_background_entered", Callable(self, "spawn_next"))
 	instance.connect("on_background_exited", Callable(self, "remove_sprite"))
-	
-
-func remove_previous():
-	var first = get_sprites()[0]
-	
-	if !first:
-		return
-	
-	first.queue_free()
 
 func get_bg_length() -> int:
 	return texture.get_width()
@@ -44,9 +30,7 @@ func get_sprites() -> Array[Sprite2D]:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Deberia añadir una primera copia del fondo al final
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
