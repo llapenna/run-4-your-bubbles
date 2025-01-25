9d9fa3b40474fb21645dab7cbd9dc6f7263b5f02
Kommie-o <agrafefe@gmail.com>
2025-01-25 14:11:01 -0300

    Menu Principal
    Estuve 2 horas con esto

---
+++ Scripts/menu_principal.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/__juego__.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
