9e4fa7b25e2f23671c3a85fe5605cdc6453b1d2d
Luciano Lapenna <lucianoglapenna@gmail.com>
2025-01-25 15:01:33 -0300

    layer and movement

--- background/movement.gd
+++ background/movement.gd
@@ -16,5 +16,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("speed", get_speed())
 	sprite.position.x -= get_speed()
