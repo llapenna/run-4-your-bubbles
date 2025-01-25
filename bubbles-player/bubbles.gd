f90601259c16ab59c99599948ec29d3ba6f029ac
takihama <danielcalderonuns@gmail.com>
2025-01-25 14:59:12 -0300

    bubble scale and avg

--- bubbles-player/bubbles.gd
+++ bubbles-player/bubbles.gd
@@ -2,34 +2,56 @@ extends Node2D
 
 var bubbleScene = preload("res://bubbles-player/single-bubble/Bubble.tscn")
 
+var averagePosition = Vector2(0, 0)
 var bubbles = []
-var rangeX: Vector2
-var rangeY: Vector2
+var rangeX
+var rangeY
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("print")
+	init([0, 600], [0, 1200])
+	_push(10)
+	$Timer.start()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func _init(parentRangeX: Vector2, parentRangeY: Vector2):
+func init(parentRangeX: Array, parentRangeY: Array):
 	rangeX = parentRangeX
 	rangeY = parentRangeY
 	pass
 
-func _pop(bubbleCount, timeToPopAll):
+func _pop(bubbleCount, timeToPopALl):
 	for i in bubbleCount:
-		await get_tree().create_timer(timeToPopAll/bubbleCount).timeout
+		await get_tree().create_timer(timeToPopALl/bubbleCount).timeout
 		var bubbleToPop = bubbles.pop_front()
 		$".".remove_child(bubbleToPop)
 	pass
 
 func _push(bubbleCount):
+	var averagePosition = get_average_position(bubbles.map(b => b.position))
 	for i in bubbleCount:
 		var bubbleInstance = bubbleScene.instantiate()
-		bubbleInstance.init(rangeX, rangeY)
+		
+		var randPos = Vector2(randi_range(rangeX[0], rangeX[1]), randi_range(rangeY[0], rangeY[1]))
+		var randScaleNumber = randi_range(0.2, 2.5)
+		var randScale = Vector2(randScaleNumber, randScaleNumber)
+		
+		bubbleInstance.init(randPos, randScale)
 		$".".add_child(bubbleInstance)
 		bubbles.append(bubbleInstance)
 	pass
+
+func get_average_position(points):
+	var sum: Vector2 = Vector2.ZERO
+	for point in points:
+		sum += point
+	return sum / points.size()
+
+func _on_timer_timeout() -> void:
+	print("timeout")
+	_pop(5, 5)
+	pass # Replace with function body.
--- bubbles-player/single-bubble/bubble.gd
+++ bubbles-player/single-bubble/bubble.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var initial_position = Vector2(0, 0)
+var initial_scale = Vector2(1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = initial_position
+	scale = initial_scale
 	pass # Replace with function body.
 
 
@@ -12,5 +14,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func init(parent_position):
+func init(parent_position, parent_scale):
 	initial_position = parent_position
+	initial_scale = parent_scale
