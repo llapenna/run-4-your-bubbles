be3cf885cf0cd37789bfe7109c1c2426342b5782
takihama <danielcalderonuns@gmail.com>
2025-01-25 20:01:53 -0300

    bubbles

--- bubbles-player/bubbles.gd
+++ bubbles-player/bubbles.gd
@@ -1,57 +1,66 @@
 extends Node2D
 
-var bubbleScene = preload("res://bubbles-player/single-bubble/Bubble.tscn")
+var bubbleScene = preload("res://bubbles-player/single-bubble/bubble.tscn")
 
-var averagePosition = Vector2(0, 0)
 var bubbles = []
-var rangeX
-var rangeY
+var posOrigin
+var posOffsetX: float = 0
+var posOffsetY: float = 0
+var bubbleScale = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("print")
-	init([0, 600], [0, 1200])
-	_push(10)
-	$Timer.start()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func init(parentRangeX: Array, parentRangeY: Array):
-	rangeX = parentRangeX
-	rangeY = parentRangeY
+func init(origin: Vector2, offsetX: float, offsetY: float, parentBubbleScale: float):
+	posOrigin = origin
+	posOffsetX = offsetX
+	posOffsetY = offsetY
+	bubbleScale = parentBubbleScale
 	pass
 
-func _pop(bubbleCount, timeToPopALl):
+func popBubbles(bubbleCount, timeToPopALl):
 	for i in bubbleCount:
-		await get_tree().create_timer(timeToPopALl/bubbleCount).timeout
 		var bubbleToPop = bubbles.pop_front()
+		bubbleToPop.explode()
+		await get_tree().create_timer(timeToPopALl/bubbleCount).timeout
+		
 		$".".remove_child(bubbleToPop)
 	pass
 
-func _push(bubbleCount):
-	var averagePosition = get_average_position(bubbles.map(b => b.position))
+func pushBubbles(bubbleCount):
+	var bubblePoints = []
+	
+	for bubble in bubbles:
+		bubblePoints.append(bubble.position)
+	
 	for i in bubbleCount:
 		var bubbleInstance = bubbleScene.instantiate()
+
+		var randPos = Vector2(
+			randfn(posOrigin.x, posOffsetX), 
+			randfn(posOrigin.y, posOffsetY)
+		)
 		
-		var randPos = Vector2(randi_range(rangeX[0], rangeX[1]), randi_range(rangeY[0], rangeY[1]))
-		var randScaleNumber = randi_range(0.2, 2.5)
+		var distance = randPos.distance_to(posOrigin)
+		
+		var randScaleNumber = 1*bubbleScale
+		# var randScaleNumber = clamp(bubbleScale*expexpbubbleScale/(distance*distance), bubbleScale*0.15, bubbleScale)
 		var randScale = Vector2(randScaleNumber, randScaleNumber)
 		
 		bubbleInstance.init(randPos, randScale)
 		$".".add_child(bubbleInstance)
 		bubbles.append(bubbleInstance)
-	pass
 
 func get_average_position(points):
 	var sum: Vector2 = Vector2.ZERO
+	if (points.size() == 0):
+		return sum;
+
 	for point in points:
 		sum += point
 	return sum / points.size()
-
-func _on_timer_timeout() -> void:
-	print("timeout")
-	_pop(5, 5)
-	pass # Replace with function body.
--- bubbles-player/single-bubble/bubble.gd
+++ bubbles-player/single-bubble/bubble.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
 var initial_position = Vector2(0, 0)
-var initial_scale = Vector2(1, 1)
+var initial_scale = Vector2(0.005, 0.005)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = initial_position
-	scale = initial_scale
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func init(parent_position, parent_scale):
-	initial_position = parent_position
-	initial_scale = parent_scale
+	position = parent_position
+	scale = parent_scale
+
+func explode():
+	$Area2D/AnimatedSprite2D.play("default")
