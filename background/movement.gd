2213341e578bbba29326fad1b3a92f245967b90e
Luciano Lapenna <lucianoglapenna@gmail.com>
2025-01-26 11:58:22 -0300

    adjust bacgrkounds to bigger images

--- background/movement.gd
+++ background/movement.gd
@@ -4,11 +4,20 @@ signal on_background_entered(sprite)
 signal on_background_exited(sprite)
 
 @onready var sprite: Node2D = $"."
+@onready var screenDetector: VisibleOnScreenNotifier2D = $"./VisibleOnScreenNotifier2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var texture = get_parent().texture
-	$".".texture = texture
+	var resize = get_parent().resize
+
+	sprite.texture = texture
+	sprite.scale = Vector2(resize, resize)
+
+	# No need to apply resize here because screenDetector is already scaled by being
+	# child of Sprite2D
+	var length = texture.get_width()
+	screenDetector.rect = Rect2(-length / 2, 0, length, 100)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,8 +25,10 @@ func _process(delta: float) -> void:
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	print("spawn next")
 	on_background_entered.emit(self)
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	print("remove")
 	on_background_exited.emit(self)
