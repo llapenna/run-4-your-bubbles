2341c6b131965c9e4de286dfb46a14b898579d3d
rsuriano <ramirosuriano@gmail.com>
2025-01-26 13:02:28 -0300

    player anim

--- player/player.gd
+++ player/player.gd
@@ -5,15 +5,11 @@ var jump_speed = speed * 40
 var anim_speed = speed * 0.1
 
 var can_jump = true
-
 var is_ducking = false
-var is_sliding = false
-var is_getting_up = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$AnimationPlayer.play("run")  # Start with the "run" animation as default.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -21,40 +17,44 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	velocity.y += get_gravity().y * delta
 
-
-func get_input():	
+func get_input():    
 	var jump = Input.is_action_just_pressed("jump")
 	var slide_down = Input.is_action_just_pressed("duck")
 	var slide_up = Input.is_action_just_released("duck")
 	
-	
-	if jump and can_jump:
-		can_jump = false
-		$Anim.play("jump", anim_speed*0.5)
-		velocity.y -= jump_speed
-		is_ducking = false
-	
-	elif slide_down and is_on_floor() and not is_sliding and not is_ducking:
-		$Anim.play("SlideDown", anim_speed)
+	# Handle jumping
+	if jump and can_jump and is_on_floor():  # Only allow jump if on the ground
+		can_jump = false  # Disable jumping until the player is on the ground again
+		$AnimationPlayer.play("jump")  # Play the "jump" animation
+		velocity.y -= jump_speed  # Apply vertical speed for jump
+		is_ducking = false  # Ensure the ducking state is reset
+
+	# Prevent ducking if already ducking
+	elif slide_down and is_on_floor() and not is_ducking:
+		$AnimationPlayer.play("SlideDown")  # Play the "SlideDown" animation
 		is_ducking = true
 		
+	# Handle getting up from ducking
 	elif slide_up and is_on_floor() and is_ducking:
-		is_getting_up = true
+		$AnimationPlayer.play("SlideUp")  # Play the "SlideUp" animation
 		is_ducking = false
-		is_sliding = false
-		$Anim.play("SlideUp")
-	
+
+	# Handle regular running state (on the ground)
 	elif is_on_floor() and not is_ducking:
-		print("onfloor")
-		can_jump = true
-		$Anim.play("run", anim_speed)
+		can_jump = true  # Allow jumping again when the player is on the floor
+		$AnimationPlayer.play("run", anim_speed)  # Play the "run" animation
 
 
-func _on_anim_animation_finished() -> void:
-	if ($Anim.animation == "SlideDown"):
-		$Anim.play("slide")
-		is_sliding = true
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == "jump":
+		#if is_on_floor():
+			#$AnimationPlayer.play("run")  # Return to "run" animation once the jump is finished
+			#can_jump = true  # Re-enable jumping when the player is back on the ground
+	
+	if anim_name == "SlideDown":
+		$AnimationPlayer.play("slide")  # Play the sliding animation
+		is_ducking = true
 		
-	if ($Anim.animation == "SlideUp"):
-		$Anim.play("run")
-		is_sliding = false
+	if anim_name == "SlideUp":
+		$AnimationPlayer.play("run")  # Return to running animation after sliding up
+		is_ducking = false
